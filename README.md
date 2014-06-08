5e7c01e80ff31e6f063a083df4139626cd7abb94
James Dennes <jdennes@gmail.com>
2014-06-08 10:49:49 +0200

    Use contribution-checker library

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-contribution-checker-app
-========================
+# GitHub Contribution Checker App
 
-An app to demonstrate usage of the [contribution-checker](https://github.com/jdennes/contribution-checker) library.
+People :heart: GitHub Contributions. However, it's not always simple to tell why a commit isn't [counted as a contribution][contributions].
+
+This app lets you authenticate with GitHub and check whether a specific commit qualifies as a contribution, by using the [contribution-checker][contribution-checker] library.
+
+Created by @izuzak and @jdennes at the [Hamburg Hackathon][hamburg-hackathon], June 2014.
+
+[contributions]: https://help.github.com/articles/why-are-my-contributions-not-showing-up-on-my-profile
+[contribution-checker]: https://github.com/jdennes/contribution-checker
+[hamburg-hackathon]: http://hamburg-hackathon.de/hackathon/
--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "rest_client"
 require "json"
+require "contribution-checker"
 
 CLIENT_ID = ENV["GITHUB_CLIENT_ID"]
 CLIENT_SECRET = ENV["GITHUB_CLIENT_SECRET"]
@@ -15,7 +16,7 @@ def authenticate!
   redirect "https://github.com/login/oauth/authorize?scope=public_repo,user:email&client_id=#{CLIENT_ID}"
 end
 
-get '/' do
+get "/" do
   if !authenticated?
     authenticate!
   else
@@ -23,7 +24,7 @@ get '/' do
     scopes = []
 
     begin
-      auth_result = RestClient.get('https://api.github.com/user',
+      auth_result = RestClient.get("https://api.github.com/user",
                                    {:params => {:access_token => access_token},
                                     :accept => :json})
     rescue => e
@@ -41,17 +42,25 @@ get '/' do
     end
 
     auth_result = JSON.parse(auth_result)
-    erb :index
+    if params[:url]
+      checker = ContributionChecker::Checker.new \
+        :access_token => access_token,
+        :commit_url => params[:url]
+      result = checker.check
+      erb :result, :locals => result
+    else
+      erb :index
+    end
   end
 end
 
-get '/callback' do
-  session_code = request.env['rack.request.query_hash']['code']
-  result = RestClient.post('https://github.com/login/oauth/access_token',
+get "/callback" do
+  session_code = request.env["rack.request.query_hash"]["code"]
+  result = RestClient.post("https://github.com/login/oauth/access_token",
                           {:client_id => CLIENT_ID,
                            :client_secret => CLIENT_SECRET,
                            :code => session_code},
                            :accept => :json)
-  session[:access_token] = JSON.parse(result)['access_token']
-  redirect '/'
+  session[:access_token] = JSON.parse(result)["access_token"]
+  redirect "/"
 end
