0b6ab3f3ab34010d7141f8c9846f19fdc7b9a03d
James Dennes <jdennes@gmail.com>
2014-06-10 13:27:06 +0200

    Commit basic ajax functionality

--- public/app.js
+++ public/app.js
@@ -14,6 +14,8 @@
       if(e.which == 13) {
         $(".alert").hide();
         $("#results").hide();
+        $("input").attr("disabled", "");
+        $("#waiting").show();
         var commit_url = $("input[name=url]").val();
         $.ajax({
           type: "POST",
@@ -22,15 +24,19 @@
           dataType: "json",
           success: function(data) {
             if (data.error_message) {
-              $("#err").html(renderError(data));
-              $("#err").show();
+              $("#err").html(renderError(data)).show();
             } else {
               data.commit_url = commit_url;
-              $("#results").html(renderResults(data));
-              $("#results").show();
+              $("#results").html(renderResults(data)).show();
             }
           },
           error: function(xhr, status, error) {
+            var data = { error_message: "Something went horribly wrong..." };
+            $("#err").html(renderError(data)).show();
+          },
+          complete: function() {
+            $("#waiting").hide();
+            $("input").removeAttr("disabled");
           }
         });
       }
